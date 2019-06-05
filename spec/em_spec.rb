5f2ccbb1fc868d73e382f46a8c3d6146123940a2
ngodi <albertngodi@gmail.com>
2019-06-05 08:33:45 +0100

    removed extra describe blocks

--- spec/em_spec.rb
+++ spec/em_spec.rb
@@ -1,5 +1,5 @@
 #spec/em_spec.rb
-require './lib/enumerable-methods'  #=> add this
+require '../lib/enumerable-methods'  #=> add this
 
 
 
@@ -9,45 +9,46 @@ RSpec.describe Enumerable do
         expect([0,1,2].my_each{|n| n}).to eql([0,1,2])
       end
     end
-end
+
   
 
-RSpec.describe Enumerable do
+
     describe "#my_select" do
       it "returns the selected elements" do
         expect([0,1,2,10,100].my_select{|n| n % 2 == 0}).to eql([0,2,10,100])
       end
     end
-end
 
-RSpec.describe Enumerable do
+
+
     describe "#my_all" do
       it "returns false if no every element fills the condition" do
         expect([0,0,0,0,2].my_all?{|n| n == 0}).to eql(false)
       end
     end
-end
 
 
-RSpec.describe Enumerable do
+
+
     describe "#my_each_with_index" do
       it "returns every array element with it's index" do
           expect([9,150,6].my_each_with_index {|element, index| [element,index]}).to eql([[9,0],[150,1],[6,2]]) 
       end
   end 
-end 
 
-RSpec.describe Enumerable do
+
+
     describe "#my_any?" do
       it "returns true if any element of array satisfies condition" do
         expect([0,500,2].my_any? {|e| e % 2 == 0}).to eql(true) 
       end
     end 
-end 
 
-RSpec.describe Enumerable do
-  let(:test_array) { [9,2,4] }
+
+
+  
   describe"#my_count" do
+    let(:test_array) { [9,2,4] }
      it "counts total array elements" do
     expect(test_array.my_count{|item| item +=1 }).to eql(3) 
     end
@@ -55,9 +56,6 @@ RSpec.describe Enumerable do
     expect(test_array.my_count{|item| item +=1 if item >5}).to eql(1) 
     end
   end 
+
 end 
 
-
-=begin 
-
-=end 
